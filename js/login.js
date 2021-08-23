e01273c43c92a91e78961073a66812ddb86ac564
vivimac <vivimax26@gmail.com>
2021-08-22 22:11:49 -0300

    Entrega 1- login y listado de productos

--- js/login.js
+++ js/login.js
@@ -10,7 +10,7 @@ function verificar() {
   let usuario = {};
 
   if (dato.value.trim() === "" || pass.value.trim() === "") {
-    dato.classList.add("isInvalid");
+    dato.classList.add("isInvalid") || pass.classList.add("isInvalid") ;
     msj.innerHTML = "Dato requerido";
     msj.style.color = "red";
     msj.style.display = "block";
@@ -20,19 +20,18 @@ function verificar() {
     usuario.nombre = dato.value;
     usuario.estado = "conectado";
 
-    localStorage.setItem("usuario", JSON.stringify(usuario));
-    sessionStorage.setItem("usuario", JSON.stringify(usuario));
+    
   }
 }
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
 
   /*console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-                         console.log('Full Name: ' + profile.getName());
-                         console.log('Given Name: ' + profile.getGivenName());
-                         console.log('Family Name: ' + profile.getFamilyName());
-                         console.log("Image URL: " + profile.getImageUrl());
-                         console.log("Email: " + profile.getEmail());*/
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());*/
 
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
--- js/products.js
+++ js/products.js
@@ -1,6 +1,39 @@
+let productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.desc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1"><b>`+ product.name +`</h4></b>
+                        <small class="text-muted"><b>` + product.currency + product.cost + `</small></b>
+                    </div>
+                    <p class="mb-1">`+ product.description +`<p>
+                </div>
+            </div>
+        </div> 
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function(e){
 
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
 });
