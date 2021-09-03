fbf45f05b9b2e4fd8efa86335099253d099fd65b
vivimac <vivimax26@gmail.com>
2021-09-03 19:27:54 -0300

    Filtros en html de products, nombre de usuario y desconectar en todas las paginas

--- js/login.js
+++ js/login.js
@@ -16,25 +16,23 @@ function verificar() {
     msj.style.display = "block";
 
   } else {
-    location.href = "principal.html";
 
     usuario.nombre = dato.value;
     usuario.estado = "conectado";
+    localStorage.setItem('usuario',JSON.stringify(usuario));
     sessionStorage.setItem('usuario',JSON.stringify(usuario));
+    
+    location.href = "principal.html";
 
   }}
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
 
-  /*console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());*/
-  
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-  location.href = "principal.html"; }
+  document.addEventListener('DOMContentLoaded', ()=>{
+    let usuario = JSON.parse( localStorage.getItem("usuario"));
+        document.getElementById("nombre").innerHTML=  usuario.nombre;
+});
+
+function desconectar(){
+localStorage.clear(); 
+location.href="index.html"; 
+}
--- js/products.js
+++ js/products.js
@@ -1,39 +1,56 @@
 let productsArray = [];
 
-function showProductsList(array){
+function showProductsList(array) {
+  
+  let htmlContentToAppend = "";
+  for (let i = 0; i < array.length; i++) {
+    let product = array[i];
 
-    let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let product = array[i];
-
-        htmlContentToAppend += `
+    htmlContentToAppend +=
+      `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.desc + `" class="img-thumbnail">
+                    <img src="` +
+      product.imgSrc +
+      `" alt="` +
+      product.desc +
+      `" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1"><b>`+ product.name +`</h4></b>
-                        <small class="text-muted"><b>` + product.currency + product.cost + `</small></b>
-                    </div>
-                    <p class="mb-1">`+ product.description +`<p>
+                    <h4 class="mb-1"><b>` +
+      product.name +
+      `</h4></b></div>
+                    <div><p class="mb-1">` +
+      product.description +
+      `<p> </div>
+                       <div> <small class="text-muted"><b>` +
+      product.currency +
+      product.cost +
+      `</small></b></div>
+      <div><br><small class="text">` +
+      product.soldCount +
+      ` artículos vendidos </small>
+                       </div>
+                    
                 </div>
             </div>
         </div> 
-        `
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
-    }}
+        `;
+
+    document.getElementById("cat-list-container").innerHTML =
+      htmlContentToAppend;
+  }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            productsArray = resultObj.data;
-            showProductsList(productsArray);
-        }
-    });
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      productsArray = resultObj.data;
+      showProductsList(productsArray);
+    }
+  });
 });
